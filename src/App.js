5e3df1c43ed3caa660d39dc680c0977eb0bce625
sophieespejo <sophmeas@gmail.com>
2022-03-14 13:04:10 -0700

    set up navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import logo from "./Assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar sticky="top" className="navbarBG">
+          <Container className="justify-content-center d-flex flex-wrap">
+            <Row className="justify-content-center pb-0 mb-0">
+              <Navbar.Brand className=" text-center" href="/">
+                <img
+                  src= {logo}
+                  className="img-fluid"
+                />
+              </Navbar.Brand>
+            </Row>
+            <Row className="">
+              <Navbar.Toggle />
+              <Navbar.Collapse className="justify-content-center">
+                <Nav>
+                  <Nav.Link as={Link} to="/">
+                    Project Dashboard{" "}
+                  </Nav.Link>
+
+                  <Nav.Link as={Link} to="/dashboard">
+                    {" "}
+                    Login/Create an Account{" "}
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/createAcct">
+                    Sign Out 
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Row>
+          </Container>
+        </Navbar>
+      </BrowserRouter>
+    </>
   );
 }
 
