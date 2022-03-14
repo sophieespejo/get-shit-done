98ec30c4584ffd7f7ba4b2b68784bff00cd6c9e6
sophieespejo <sophmeas@gmail.com>
2022-03-14 11:20:47 -0700

    installed router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
