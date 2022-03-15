c2b4d42f44ff06039609e2e64df4e731c89fe996
sophieespejo <sophmeas@gmail.com>
2022-03-15 10:58:12 -0700

    changed input to text

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -18,10 +18,7 @@ const Login = () => {
                         <Form>
                             <Form.Group className="mb-3 " controlId="formBasicEmail">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
+                                <Form.Control type="text" placeholder="Enter username" />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
