a729ea52e63bf23ef2f0b37abf1ba93f1c905bd0
Daniel Bautista <dbautista9015@gmail.com>
2022-03-15 09:48:08 -0700

    Created Layout for login and create

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './Pages/Login';
+import CreateAccount from './Pages/CreateAccount';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path='createaccount' element={<CreateAccount />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/Pages/CreateAccount.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Outlet, Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <h1>Create an account</h1>
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col className="d-flex justify-content-center mt-3">
+                        <Form>
+                            <Form.Group className="mb-3 " controlId="formBasicEmail">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                                {/* <Link to="../">Login</Link> */}
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                submit
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            
+        </>
+
+    )
+}
+
+export default Login;
