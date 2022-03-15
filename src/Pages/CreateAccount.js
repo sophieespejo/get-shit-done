af8e39b93525b755940a1fb01b88a2a12f702029
sophieespejo <sophmeas@gmail.com>
2022-03-15 10:56:16 -0700

    changed email input to text

--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -18,12 +18,8 @@ const Login = () => {
                         <Form>
                             <Form.Group className="mb-3 " controlId="formBasicEmail">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
+                                <Form.Control type="text" placeholder="Enter username" />
                             </Form.Group>
-
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
@@ -36,7 +32,6 @@ const Login = () => {
                     </Col>
                 </Row>
             </Container>
-
         </>
 
     )
