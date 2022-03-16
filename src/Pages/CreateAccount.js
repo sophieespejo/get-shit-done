74e417076c7e466c9bfb14b708bbd8be009cf25f
Daniel Bautista <dbautista9015@gmail.com>
2022-03-16 11:26:26 -0700

    Trying to Connect create account

--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -1,44 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { createAccount } from '../Services/DataService';
 
 const Login = () => {
-    return (
-        <>
-            <Container>
-                <Row>
-                    <Col className="d-flex justify-content-center">
-                        <h1>Create an account</h1>
-                    </Col>
-                </Row>
-            </Container>
-            <Container>
-                <Row>
-                    <Col className="d-flex justify-content-center mt-3">
-                        <Form>
-                            <Form.Group className="mb-3 " controlId="formBasicEmail">
-                                <Form.Label>Full Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Full Name" />
-                            </Form.Group>
-                            <Form.Group className="mb-3 " controlId="formBasicEmail">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                                {/* <Link to="../">Login</Link> */}
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                submit
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </>
 
-    )
-}
+    let navigate = useNavigate();
 
-export default Login;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [fullname, setFullName] = useState("");
+
+    const handleSubmit = async () => {
+        let userData = {
+            Id: 0,
+            Fullname: fullname,
+            Username: username,
+            Password: password,
+        }
+        // console.log(userData);
+        let result = await createAccount(userData);
+        result ? (
+            navigate("/Login")
+        ) : alert("Unable to create an account. Please try again.")
+    };
+
+        return (
+            <>
+                <Container>
+                    <Row>
+                        <Col className="d-flex justify-content-center">
+                            <h1>Create an account</h1>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col className="d-flex justify-content-center mt-3">
+                            <Form>
+                                <Form.Group className="mb-3 " controlId="formBasicEmail">
+                                    <Form.Label>Full Name</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter Full Name" />
+                                </Form.Group>
+                                <Form.Group className="mb-3 " controlId="formBasicEmail">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter username" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                    {/* <Link to="../">Login</Link> */}
+                                </Form.Group>
+                                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                                    submit
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+
+        )
+    }
+
+    export default Login;
---
+++ src/Services/DataService.js
@@ -0,0 +1,78 @@
+let successfulCreateAcct = false;
+let userData = {};
+
+function checkToken(){
+    let result = false;
+    let lsData = localStorage.getItem('Token');
+    if(lsData && lsData != null)
+    {
+        result = true;
+    }
+    return result;
+}
+
+//Needs to be changed later
+async function createAccount(createdUser){
+    //wait for fetch to for api
+    //http://getshitdonebackend.database.windows.net/
+    let res = await fetch('http://getshitdonebackend.database.windows.net/User/AddUser', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(createdUser)
+    });
+
+    //check status of request
+    if(!res.ok)
+    {
+        const message = `An error has occured ${res.status}`
+        throw new Error(message);
+    }
+    let data = await res.json();
+    
+    if(data)
+    {
+        successfulCreateAcct = true;
+    }
+    else{
+        successfulCreateAcct = false;
+        alert("Unable to create an account. Please try again");
+    }
+    return successfulCreateAcct;
+}
+
+//Needs to be changed for Log in, check if works
+async function logIn(userInfo){
+
+    let res = await fetch('http://getshitdonebackend.database.windows.net/User/Login', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    });
+    //check status of request
+    if(!res.ok)
+    {
+        const message = `An error has occured ${res.status}`;
+        alert("Username and/or Password is incorrect. Please try again or create a new account.");
+        throw new Error(message);
+    }
+    let data = await res.json();
+    // console.log(data.token);
+    // if(!data.token)
+    // {
+    //     successfulLogin = false;
+    //     alert("unable to login");
+    // }
+    // else{
+    //     successfulLogin = true;
+    //     localStorage.setItem('Token', JSON.stringify(data.token));
+    // }
+    // console.log(successfulLogin);
+    // return successfulLogin;
+    return data;
+}
+
+export { createAccount }
