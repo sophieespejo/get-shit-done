00620d5db4d993937afda5d2c9136cc9059c88a1
sophieespejo <sophmeas@gmail.com>
2022-03-17 08:23:39 -0700

    added styling to login and createAcct pages

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -10,31 +10,34 @@ const Login = () => {
         <>
             <Container>
                 <Row>
-                    <Col className="d-flex justify-content-center">
+                    <Col className="d-flex justify-content-center mt-5 loginTxt">
                         <h1>Login</h1>
                     </Col>
                 </Row>
             </Container>
-            <Container>
-                <Row>
-                    <Col className="d-flex justify-content-center mt-3">
+            <Container >
+                <Row className="justify-content-center" >
+                    <Col xs={3} className="d-flex justify-content-center mt-3 pt-3 pb-3 loginContainer">
                         <Form>
-                            <Form.Group className="mb-3 " controlId="formBasicEmail">
+                            <Form.Group className="mb-3 loginTxt" controlId="formBasicEmail">
                                 <Form.Label>Username</Form.Label>
                                 <Form.Control type="text" placeholder="Enter username" />
                             </Form.Group>
                             {/* span contentEditable onKeyDown={(e) => console.log(e.target.textContent)} */}
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Group className="mb-3 loginTxt" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
-                                <Link to="/createaccount">Create an account</Link>
                             </Form.Group>
-                            
-                            <Button variant="primary" type="submit">
-                                Login
-                            </Button>
-                            
-                            
+                            <Container>
+                                <Row className="d-grid gap-2 mt-2 mb-2 ">
+                                    <Button variant="primary" size="sm" className="editBtn" type="submit">
+                                        Login
+                                    </Button>
+                                </Row>
+                            </Container>
+                            <Row>
+                                <Link to="/createaccount" className="text-center">Create an account</Link>
+                            </Row>
                         </Form>
                     </Col>
                 </Row>
