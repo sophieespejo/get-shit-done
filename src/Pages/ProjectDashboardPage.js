65b1d200e0c0b38e8c9e4e51b84e66ca3d3ba09a
Danial <Danial.Dad248@gmail.com>
2022-03-16 22:22:32 -0700

    fixed merge conflicts

--- src/Pages/ProjectDashboardPage.js
+++ src/Pages/ProjectDashboardPage.js
@@ -1,10 +1,11 @@
-import React, { useState, useContext } from 'react'
-import { Container, Row, Col, Accordion, ListGroup, Button } from "react-bootstrap";
-import ProjectCardComponent from '../Components/ProjectCardComponent';
-import NewProjectComponent from '../Components/NewProjectComponent';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import UserContext from '../Context/UserContext';
+// import React, { useState, useContext } from 'react'
+// import { Container, Row, Col, Accordion, ListGroup, Button } from "react-bootstrap";
+// import ProjectCardComponent from '../Components/ProjectCardComponent';
+// import NewProjectComponent from '../Components/NewProjectComponent';
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import UserContext from '../Context/UserContext';
+
 import React, { useState, useEffect } from "react";
 import {
   Container,
@@ -49,23 +50,6 @@ export default function ProjectDashboardPage() {
       
   }, [])
 
-  const [blogItems, setBlogItems] = useState([
-    {
-      Id: 1,
-      isPublished: true,
-      isArchived: true,
-      isProjectManager: true,
-      isAdmin: false,
-      Title: "Growing Tomatos",
-      Publisher: "Walaa AlSalmi",
-      Date: "01-12-2022",
-      Text: "Devote a prime, sunny spot to growing tomatoes. Tomatoes need at least 6 to 8 hours of sun to bring out their best flavors. You will need to stake, trellis, or cage most tomato plants to keep them off the ground. Decide on a support plan before you set out your plants, then add that support directly after planting. You will need to stake, trellis, or cage most tomato plants to keep them off the ground. Decide on a support plan before you set out your plants.",
-      Image:
-        "https://www.almanac.com/sites/default/files/styles/landscape/public/image_nodes/tomatoes_helios4eos_gettyimages-edit.jpeg?itok=m9c3T-XV",
-    },
-
-<<<<<<< HEAD
-
   const [blogItems, setBlogItems] = useState([ {
     Id: 1,
     isArchived: true,
@@ -107,48 +91,6 @@ export default function ProjectDashboardPage() {
     Image:
       "https://greenhouseemporium.com/wp-content/uploads/2020/02/How_to_Grow_Zucchini_2.jpg",
   }])
-=======
-    {
-      Id: 2,
-      isArchived: true,
-      isPublished: true,
-      isProjectManager: true,
-      isAdmin: false,
-      Title: "Growing Peppers",
-      Date: "01-06-2022",
-      Publisher: "Tom Finland",
-      Text: "Set pepper plant seedlings out after the last spring frost. They grow well in raised beds, containers, and in-ground gardens. Plant them 18 to 24 inches apart in a sunny, well-drained spot. Pepper plants need at least 6-8 hours of sunlight per day. They grow well in raised beds, containers, and in-ground gardens. Plant them 18 to 24 inches apart in a sunny, well-drained spot. Pepper plants need at least 6-8 hours of sunlight per day.",
-      Image:
-        "https://www.farmersalmanac.com/wp-content/uploads/2020/11/Planting-Guide-Bell-Peppers-A105431708.jpg",
-    },
-    {
-      Id: 3,
-      isArchived: true,
-      isPublished: true,
-      isProjectManager: false,
-      isAdmin: true,
-      Title: "Growing Eggplants",
-      Publisher: "Sam Bilton",
-      Date: "12-24-2021",
-      Text: "Start eggplant seeds indoors up to 10 weeks before the last frost date. Plant the seeds 1/4inch deep, water after planting and cover loosely with plastic to retain moisture. Transplant the seedlings to the garden when soil temperatures reach 60 degrees. Transplant the seedlings to the garden when soil temperatures reach 60 degrees.",
-      Image:
-        "https://cleangreensimple.com/wp-content/uploads/2020/05/growing-eggplant.jpg",
-    },
-    {
-      Id: 4,
-      isArchived: true,
-      isPublished: true,
-      isProjectManager: false,
-      isAdmin: true,
-      Title: "Growing Zucchinis",
-      Publisher: "Tina Freedman",
-      Date: "12-15-2021",
-      Text: "Zucchini needs full sun (at least 6 to 8 hours) and consistently moist soil that is high in organic matter. Some zucchini varieties are vining types that require a trellis or a lot of room to sprawl. There are also bush types suitable for container gardening and small space gardening. There are also bush types suitable for container gardening and small space gardening.",
-      Image:
-        "https://greenhouseemporium.com/wp-content/uploads/2020/02/How_to_Grow_Zucchini_2.jpg",
-    },
-  ]);
->>>>>>> 1e68afcb98cf0fd628a1ab7c7563f0f4667113c0
 
   const { viewIcon } = <FontAwesomeIcon icon={faMagnifyingGlass} />;
   return (
