d15f0171644e0904b68e32beb829d6bbe4416d54
sophieespejo <sophmeas@gmail.com>
2022-03-21 15:41:48 -0700

    added checktoken and spinner

--- src/Pages/ProjectDashboardPage.js
+++ src/Pages/ProjectDashboardPage.js
@@ -9,6 +9,7 @@ import {
   Modal,
   Form,
   Card,
+  Spinner,
 } from "react-bootstrap";
 import ProjectCardComponent from "../Components/ProjectCardComponent";
 import NewProjectComponent from "../Components/NewProjectComponent";
@@ -38,6 +39,8 @@ import ProjectContext from "../Context/ProjectContext";
 import TaskContext from "../Context/TaskContext";
 
 export default function ProjectDashboardPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
   let userData = useContext(UserContext);
   let clickedProject1 = useContext(ProjectContext);
   // console.log(userData.userItems)
@@ -90,7 +93,6 @@ export default function ProjectDashboardPage() {
   const handleClose1 = () => setShow1(false);
   const handleShow1 = () => setShow1(true);
 
-
   const [allUsers, setAllUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState({});
 
@@ -111,8 +113,8 @@ export default function ProjectDashboardPage() {
     let updatedMembers = [];
     splitArr.push(stringId);
     updatedMembers = splitArr;
-    
-    let updatedProject = { 
+
+    let updatedProject = {
       Id: currentClickedProject.id,
       UserId: currentClickedProject.userId,
       Title: projectTitle,
@@ -120,11 +122,11 @@ export default function ProjectDashboardPage() {
       DateCreated: currentClickedProject.dateCreated,
       DueDate: projectDueDate,
       Status: "test",
-      MembersId: updatedMembers.join(','),
+      MembersId: updatedMembers.join(","),
       MembersUsername: "test",
       IsDeleted: currentClickedProject.isDeleted,
       IsArchived: currentClickedProject.isArchived,
-    }
+    };
     updateProjectItem(updatedProject);
     handleClose();
   };
@@ -151,11 +153,11 @@ export default function ProjectDashboardPage() {
       DateCreated: currentClickedProject.dateCreated,
       DueDate: projectDueDate,
       Status: "test",
-      MembersId: updatedMembers.join(',').toString(),
+      MembersId: updatedMembers.join(",").toString(),
       MembersUsername: "test",
       IsDeleted: currentClickedProject.isDeleted,
       IsArchived: currentClickedProject.isArchived,
-    }
+    };
     updateProjectItem(updatedProject);
     handleClose();
   };
@@ -198,53 +200,45 @@ export default function ProjectDashboardPage() {
     setProjectDueDate(project.dueDate);
     setShow2(true);
 
-  // const [isArchived, setIsArchived] = useState(false);
-  allFetchedUsers = await getAllUsers();
-  setAllSpecialist(allFetchedUsers.filter((user) => user.isSpecialist));
-};
+    // const [isArchived, setIsArchived] = useState(false);
+    allFetchedUsers = await getAllUsers();
+    setAllSpecialist(allFetchedUsers.filter((user) => user.isSpecialist));
+  };
 
-const handleArchived = async (project) => {
-  project.isArchived = true;
-  let result = await updateProjectItem(project);
-  if(result)
-  {
-    let projects = await getAllProjectItems();
-    setCurrentProjects(projects);
-  }
-  else{
-    alert(`A project has not been archived`)
-  }
-}
-
+  const handleArchived = async (project) => {
+    project.isArchived = true;
+    let result = await updateProjectItem(project);
+    if (result) {
+      let projects = await getAllProjectItems();
+      setCurrentProjects(projects);
+    } else {
+      alert(`A project has not been archived`);
+    }
+  };
 
   useEffect(async () => {
-    allFetchedUsers = await getAllUsers();
-    setAllUsers(allFetchedUsers);
+    if (!checkToken()) {
+      navigate("/Login");
+    } else {
+      allFetchedUsers = await getAllUsers();
+      setAllUsers(allFetchedUsers);
 
-    setTimeout(async () => {
-      let currentFetchedProjects;
-      console.log(userData.userItems);
-
-      if (userData.userItems.isSpecialist) {
-        currentFetchedProjects = await getProjectItemsByAMemberId(userItems.id);
-        console.log("specialist");
-      } else if (userData.userItems.isProjectManager) {
-        currentFetchedProjects = await getProjectItemsByUserId(userItems.id);
-        console.log("pm");
-      } else if (userData.userItems.isAdmin) {
-        currentFetchedProjects = await getAllProjectItems();
-        console.log("admin");
-
-        console.log(userData.userItems);
-      }
-      
-      setCurrentProjects(currentFetchedProjects);
-
-    }, 3000);
-
-  }, [])
-
-
+      setTimeout(async () => {
+        let currentFetchedProjects;
+        if (userData.userItems.isSpecialist) {
+          currentFetchedProjects = await getProjectItemsByAMemberId(
+            userItems.id
+          );
+        } else if (userData.userItems.isProjectManager) {
+          currentFetchedProjects = await getProjectItemsByUserId(userItems.id);
+        } else if (userData.userItems.isAdmin) {
+          currentFetchedProjects = await getAllProjectItems();
+        }
+        setCurrentProjects(currentFetchedProjects);
+        setIsLoading(false);
+      }, 3000);
+    }
+  }, []);
 
   //Function to show model when edit button is clicked
 
@@ -253,137 +247,149 @@ const handleArchived = async (project) => {
     <>
       <Container className="mt-5">
         <h4 className="headerTxt">Your Current Projects: </h4>
-        <Row xs={2} lg={4} className="g-3">
-          {userData.userItems.isAdmin || userData.userItems.isProjectManager ? (
-            <NewProjectComponent />
-          ) : null}
-          {/* Map thru current projects here */}
-          <Modal show={show2} onHide={handleClose2}>
-            <Modal.Header closeButton>
-              <Modal.Title>Edit Project</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Project title:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Edit project title"
-                    onChange={(e) => setProjectTitle(e.target.value)}
-                    value={projectTitle}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="">
-                  <Form.Label>Description:</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    type="description"
-                    placeholder="Edit description"
-                    onChange={(e) => setProjectDescription(e.target.value)}
-                    value={projectDescription}
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Due Date:</Form.Label>
-                  <Form.Control
-                    type="date"
-                    placeholder="Edit due date"
-                    onChange={(e) => setProjectDueDate(e.target.value)}
-                    value={projectDueDate}
-                  />
-                </Form.Group>
-                <Form.Label>Edit Specialist:</Form.Label>
-                <ListGroup as="ul">
-                  {allSpecialist.map((user, idx) => {
-                    // debugger
-                    if (currentClickedProject.membersId.includes(user.id)) {
-                      return (
-                        <ListGroup.Item
-                          active
-                          action
-                          as="li"
-                          onClick={(e) => handleRemoveMember(e, user.id)}
-                        >
-                          {user.fullName}
-                        </ListGroup.Item>
-                      );
-                    } else {
-                      return (
-                        <ListGroup.Item
-                          action
-                          as="li"
-                          onClick={(e) => handleAddMember(e, user.id)}
-                        >
-                          {user.fullName}
-                        </ListGroup.Item>
-                      );
-                    }
-                  })}
-                </ListGroup>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose2}>
-                Close
-              </Button>
-              <Button variant="primary" onClick={handleClose2}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
-          </Modal>
-          {/* need function that fetches all current projects of that user, but if user is an admin will show all projects */}
-          {currentProjects.map((project, idx) => {
-            return (
-              <>
-                {!project.isArchived ? (
-                  <div>
-                    <Card
-                      border="danger"
-                      style={{ width: "15rem", height: "15rem" }}
-                      className="shadow"
-                    >
-                      <Card.Body>
-                        <Card.Title className="d-flex justify-content-between">
-                          {project.title}
-                          <Button
-                            className="editBtn"
-                            onClick={(e) => handleClick2(e, project)}
+        {isLoading ? (
+                  <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>
+        ) : (
+          <Row xs={2} lg={4} className="g-3">
+            {userData.userItems.isAdmin ||
+            userData.userItems.isProjectManager ? (
+              <NewProjectComponent />
+            ) : null}
+            {/* Map thru current projects here */}
+            <Modal show={show2} onHide={handleClose2}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Project</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Project title:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Edit project title"
+                      onChange={(e) => setProjectTitle(e.target.value)}
+                      value={projectTitle}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="">
+                    <Form.Label>Description:</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      type="description"
+                      placeholder="Edit description"
+                      onChange={(e) => setProjectDescription(e.target.value)}
+                      value={projectDescription}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Due Date:</Form.Label>
+                    <Form.Control
+                      type="date"
+                      placeholder="Edit due date"
+                      onChange={(e) => setProjectDueDate(e.target.value)}
+                      value={projectDueDate}
+                    />
+                  </Form.Group>
+                  <Form.Label>Edit Specialist:</Form.Label>
+                  <ListGroup as="ul">
+                    {allSpecialist.map((user, idx) => {
+                      // debugger
+                      if (currentClickedProject.membersId.includes(user.id)) {
+                        return (
+                          <ListGroup.Item
+                            active
+                            action
+                            as="li"
+                            onClick={(e) => handleRemoveMember(e, user.id)}
                           >
-                            {editIcon}
-                          </Button>
-                        </Card.Title>
-                        <Card.Text>
-                          <p className="projectCardTxt">
-                            Due Date: <span>{project.dueDate}</span>
-                          </p>
-                          <p className="projectCardTxt">
-                            {project.description}
-                          </p>
-                        </Card.Text>
-                        {userData.userItems.isAdmin || userData.userItems.isProjectManager ? (
-                          <Row>
-                            <Col>
-                              <Button
-                                className="editBtn"
-                                onClick={(e) => handleClick(e, project)}
-                              >
-                                View
-                              </Button>
-                            </Col>
-                            <Col>
-                              <Button variant="info" onClick ={() => handleArchived(project)}>Archive</Button>
-                            </Col>
-                          </Row>
-                        ) : null}
-                      </Card.Body>
-                    </Card>
-                  </div>
-                ) : null}
-              </>
-            );
-          })}
-        </Row>
-        
+                            {user.fullName}
+                          </ListGroup.Item>
+                        );
+                      } else {
+                        return (
+                          <ListGroup.Item
+                            action
+                            as="li"
+                            onClick={(e) => handleAddMember(e, user.id)}
+                          >
+                            {user.fullName}
+                          </ListGroup.Item>
+                        );
+                      }
+                    })}
+                  </ListGroup>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose2}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose2}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+            {/* need function that fetches all current projects of that user, but if user is an admin will show all projects */}
+            {currentProjects.map((project, idx) => {
+              return (
+                <>
+                  {!project.isArchived ? (
+                    <div>
+                      <Card
+                        border="danger"
+                        style={{ width: "15rem", height: "15rem" }}
+                        className="shadow"
+                      >
+                        <Card.Body>
+                          <Card.Title className="d-flex justify-content-between">
+                            {project.title}
+                            <Button
+                              className="editBtn"
+                              onClick={(e) => handleClick2(e, project)}
+                            >
+                              {editIcon}
+                            </Button>
+                          </Card.Title>
+                          <Card.Text>
+                            <p className="projectCardTxt">
+                              Due Date: <span>{project.dueDate}</span>
+                            </p>
+                            <p className="projectCardTxt">
+                              {project.description}
+                            </p>
+                          </Card.Text>
+                          {userData.userItems.isAdmin ||
+                          userData.userItems.isProjectManager ? (
+                            <Row>
+                              <Col>
+                                <Button
+                                  className="editBtn"
+                                  onClick={(e) => handleClick(e, project)}
+                                >
+                                  View
+                                </Button>
+                              </Col>
+                              <Col>
+                                <Button
+                                  variant="info"
+                                  onClick={() => handleArchived(project)}
+                                >
+                                  Archive
+                                </Button>
+                              </Col>
+                            </Row>
+                          ) : null}
+                        </Card.Body>
+                      </Card>
+                    </div>
+                  ) : null}
+                </>
+              );
+            })}
+          </Row>
+        )}
       </Container>
       <Container>
         <Row className="mt-5">
